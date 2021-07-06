2fee978eddb746e6ead9ec39de718f5f0de22feb
eignnx <eignnx@gmail.com>
2021-07-05 22:59:08 -0400

    update parser to allow typechecking

--- Ty.hs
+++ Ty.hs
@@ -9,25 +9,23 @@ module Ty
 where
 
 import qualified Data.Map as M
-import           Data.Semigroup
 
 data Ty
   = ValTy String
   | FnTy [Ty] Ty
   | ModTy (M.Map String Ty) -- The type of a module
-  | NeverTy
   deriving (Show, Eq)
 
 (<:) :: Ty -> Ty -> Bool
+never <: t2 | never == neverTy = True
 t1 <: t2 | t1 == t2 = True
 -- FnTy x1 y1 <: FnTy x2 y2 = x2 <: x1 && y1 <: y2
 ModTy m1 <: ModTy m2 = m2 `M.isSubmapOf` m1
-NeverTy <: t2 = True
 _ <: _ = False
 
 -- TODO: use LANGUAGE PatternSynonyms: https://ghc.gitlab.haskell.org/ghc/doc/users_guide/exts/pattern_synonyms.html
-unitTy = ValTy "()"
-boolTy = ValTy "Bool"
-intTy  = ValTy "Int"
-textTy = ValTy "Text"
-
+neverTy = ValTy "Never"
+unitTy  = ValTy "Unit"
+boolTy  = ValTy "Bool"
+intTy   = ValTy "Int"
+textTy  = ValTy "Text"
