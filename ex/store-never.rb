caab59f40e1401f134ffc3be8400b926aeb62e32
eignnx <eignnx@gmail.com>
2021-08-07 21:03:09 -0400

    rename fn in example

--- ex/store-never.rb
+++ ex/store-never.rb
@@ -1,10 +1,10 @@
-def text_disguised_as_never[] -> Never do
+def disguised_as_never[] -> Never do
   intr.exit[];
   # ret false
 end
 
 def main[] do
   let x = 1;
-  x = text_disguised_as_never[];
+  x = disguised_as_never[];
   intr.puts[x];
 end
