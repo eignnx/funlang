49d92ab99d186d520e8c6eba65983f23f8428f37
eignnx <eignnx@gmail.com>
2021-08-14 13:32:53 -0400

    basic impl of recursive types (type ann'tions req'd)

--- ex/rec-type.rb
+++ ex/rec-type.rb
@@ -4,15 +4,20 @@ type rec IntList
 end
 
 def main[] do
-  let xs = {:Cons 1, {:Cons 2, {:Nil}}};
-  let ys = map[double, xs];
+  let x1 = {:Nil} as IntList;
+  let x2 = {:Cons 1, {:Nil}} as IntList;
+  let x3 = {:Cons 1, {:Cons 2, {:Nil}}} as IntList;
+  # let x = map[double, {:Nil}];
+  # map[double, {:Cons 1, {:Nil}}];
+  # let xs = {:Cons 1, {:Cons 2, {:Nil}}} as rec IntList;
+  # let ys = map[double, xs];
 end
 
 def map[f: [Int] -> Int, xs: rec IntList] -> rec IntList do
   match xs
     | { :Nil } => { :Nil }
     | { :Cons x, xs } => { :Cons f[x], map[f, xs] }
-  end
+  end as rec IntList
 end
 
 def double[x: Int] = x + x
